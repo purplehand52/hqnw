233bd6f240db12f8245c83faae8fc22b656f79c4
Abhay478 <abhayashankara@gmail.com>
2025-04-15 00:08:50 +0530

    gitdo:Added expt readme.

---
+++ experiments/README.md
@@ -0,0 +1,9 @@
+To run an experiment,
+
+```bash
+bash experiments/<name>/run.sh
+```
+
+where `<name>` is the name of the experiment. 
+
+The params format is `<num_clients> <num_repeaters> <rep_coeff> <gen_coeff> <client_coeff> <mean_capacity> <mean_demand>`, vary as necessary.
--- experiments/runtime/run.sh
+++ experiments/runtime/run.sh
@@ -2,7 +2,7 @@ declare DIR=experiments/runtime
 
 echo -n > $DIR/out.csv
 for num_clients in {10,15,20,25,30}; do
-    echo "${num_clients} 300 0.03 0.25 0.1 10 7" > inp-params.txt
+    echo "${num_clients} 300 0.03 0.25 0.2 10 7" > inp-params.txt
     python src/gur_solver.py runtimes
 done
 
